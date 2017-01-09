29a7dc8e57dc798f3d1a25ca4635a3430ebb0570
Heqian Liu <lhq@bu.edu>
2017-01-10 00:16:09 +0800

    add

--- public/javascript/champ.js
+++ public/javascript/champ.js
@@ -9,9 +9,9 @@ var page2 = document.getElementById('p2');
 var page3 = document.getElementById('p3');
 var rightArrow = document.getElementById('rightArrow');
 var leftArrow = document.getElementById('leftArrow');
-var content3 = document.getElementById('content1');
-var content1 = document.getElementById('content2');
-var content2 = document.getElementById('content3');
+var content1 = document.getElementById('content1');
+var content2 = document.getElementById('content2');
+var content3 = document.getElementById('content3');
 p1.style.fontSize = "30px";
 var current = "1";
 
